f6c36b76a04cb44f388d50ec285227959fddd302
Luka Trikha <lukadtrikha@gmail.com>
2022-02-04 13:08:32 -0500

    Initial Commit. Set up .tex document format.

---
+++ wk1/d2/t_test_example.R
@@ -0,0 +1,21 @@
+#Phong's Script:
+#This is our first script. It computes the standard deviation of the list on line 2.
+Dataset1<-c(1,0,2,1,2,1,2,0,4)
+xbar<-mean(Dataset1)
+Devs<-Dataset1-xbar
+Devs_sq<-Devs^2
+Devs_sq_sum<-sum(Devs_sq)
+n<-length(Dataset1)
+var_Dataset1<-Devs_sq_sum/(n-1)
+sd_Dataset1<-sqrt(var_Dataset1)
+print(sd_Dataset1)#should be 1.236033
+# Doing a t-test.
+h0 = 73
+n = 102
+s = 5
+xbar = 75
+se = s/sqrt(n)
+tscore = (xbar - h0)/se
+df = n-1
+pvalue = 1-pt(tscore, df)   #pt means student-t distribution
+print(pvalue)
